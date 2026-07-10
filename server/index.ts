435200757f46bee6675ee2cf02777f9661fbc84f
lihongtao123154 <lihongtao123154@gmail.com>
2026-07-10 13:27:34 +0800

    fix: Express 5 wildcard route syntax

--- server/index.ts
+++ server/index.ts
@@ -26,7 +26,7 @@ app.get('/api/health', (_req, res) => {
 
 const distPath = path.resolve(process.cwd(), 'dist')
 app.use(express.static(distPath))
-app.get('*', (_req, res) => {
+app.use((_req, res) => {
   res.sendFile(path.join(distPath, 'index.html'))
 })
 
