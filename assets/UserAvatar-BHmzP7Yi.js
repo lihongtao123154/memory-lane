ab03576b7820b90df57c80a86b4e5469a16e9d73
Deploy <deploy@memory-lane>
2026-07-10 12:44:26 +0800

    deploy: relative base build

---
+++ assets/UserAvatar-BHmzP7Yi.js
@@ -0,0 +1,6 @@
+import{d as a,j as e}from"./index-DjeK8Qg8.js";/**
+ * @license lucide-react v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M7.9 20A9 9 0 1 0 4 16.1L2 22Z",key:"vv11sd"}]],i=a("message-circle",n);function l({name:s,size:t="md"}){const r={sm:"w-10 h-10 text-lg",md:"w-12 h-12 text-xl",lg:"w-14 h-14 text-2xl"};return e.jsx("div",{className:`${r[t]} rounded-full bg-gradient-warm flex items-center justify-center flex-shrink-0`,children:e.jsx("span",{className:"font-serif text-night-900",children:s.charAt(0)})})}export{i as M,l as U};
