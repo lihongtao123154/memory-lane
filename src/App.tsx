04cc20f8f35bed98cd3cfa622ce29ef954eafba5
lihongtao123154 <lihongtao123154@gmail.com>
2026-07-10 12:42:39 +0800

    fix: use HashRouter, remove CI workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
